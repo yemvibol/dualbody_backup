d2e5ad00990b39f9054031520f4bd354a8d0a14e
yemvibol <44405282+yemvibol@users.noreply.github.com>
2020-09-16 22:49:01 +0900

    Update connect.js

--- js/connect.js
+++ js/connect.js
@@ -1,44 +1,55 @@
 const options = Ayame.defaultOptions;
-        options.clientId = clientId ? clientId : options.clientId;
-        if (signalingKey) {
-          options.signalingKey = signalingKey;
+options.clientId = clientId ? clientId : options.clientId;
+if (signalingKey) {
+    options.signalingKey = signalingKey;
+}
+
+
+
+let conn;
+const disconnect = () => {
+	if (conn) {
+		conn.disconnect();
+	}
+}
+let dataChannel = null;
+const label = 'dataChannel';
+const startConn = async () => {
+	
+	options.video.codec = videoCodec;
+	
+	conn = Ayame.connection(signalingUrl, roomId, options, true);
+    conn.on('open', async (e) => {
+        dataChannel = await conn.createDataChannel(label);
+        if (dataChannel) {
+            dataChannel.onmessage = onMessage;
         }
-        let conn;
-        const disconnect = () => {
-          if (conn) {
-            conn.disconnect();
-          }
+    });
+    conn.on('datachannel', (channel) => {
+		if (!dataChannel) {
+            dataChannel = channel;
+            dataChannel.onmessage = onMessage;
         }
-        let dataChannel = null;
-        const label = 'dataChannel';
-        const startConn = async () => {
-          conn = Ayame.connection(signalingUrl, roomId, options, true);
-          conn.on('open', async (e) => {
-            dataChannel = await conn.createDataChannel(label);
-            if (dataChannel) {
-              dataChannel.onmessage = onMessage;
-            }
-          });
-          conn.on('datachannel', (channel) => {
-            if (!dataChannel) {
-              dataChannel = channel;
-              dataChannel.onmessage = onMessage;
-            }
-          });
-          conn.on('disconnect', (e) => {
-            console.log(e);
-            dataChannel = null;
-          });
-          await conn.connect(null);
-        };
-        
+    });
+    conn.on('disconnect', (e) => {
+        console.log(e);
+        dataChannel = null;
+    });
+	const mediaStream = await navigator.mediaDevices.getUserMedia({audio: true, video: false});
+    await conn.connect(mediaStream, null);
+	
+	conn.on('open', ({authzMetadata}) => console.log(authzMetadata));
+    conn.on('addstream', (e) => {
+        document.querySelector('#remote-video').srcObject = e.stream;
+    });
+	
+};
 <!-- const sendData = () => { -->
 function sendSpData() {
     const data = document.getElementById('getresult').innerHTML + ' \n';	  
 	console.log(data);
     if (dataChannel && dataChannel.readyState === 'open') {
         dataChannel.send(data);
-	console.log(data);
     }
 };
 document.querySelector("#roomIdInput").value = roomId;
